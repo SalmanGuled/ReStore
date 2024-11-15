0cafaea9851782df7793f38c56bf89f9540bf831
Sal Guled <SalmanGuled2@gmail.com>
2024-11-15 16:56:36 -0500

    Section 4 in Progress

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,41 @@
+import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import Header from "./Header";
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
+
+function App() {
+
+  const [darkMode, setDarkMode] = useState(false);
+  const palleteType = darkMode ? 'dark' : 'light';
+  const theme = createTheme({
+    palette: {
+      mode: palleteType,
+      background: {
+        default: palleteType === 'light' ? '#eaeaea': '#121212'
+      }
+    }
+  })
+
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
+
+        <Container>
+          <Outlet />
+        </Container>
+
+      </ThemeProvider>
+
+
+
+    </>
+  )
+}
+
+export default App
---
+++ client/src/features/catalog/ProductList.tsx
@@ -0,0 +1,27 @@
+import { Grid2 } from "@mui/material";
+import { Product } from "../../app/models/product";
+import ProductCard from "./ProductCard";
+
+interface Props {
+    products: Product[];
+}
+
+
+export default function ProductList({ products }: Props) {
+
+
+    return (
+
+        <Grid2 container spacing={4}>
+            {products.map(product => (
+                <Grid2 size={3} key={product.id}>
+                    <ProductCard key={product.id} product={product} />
+
+                </Grid2>
+
+            ))}
+        </Grid2>
+
+    )
+
+}

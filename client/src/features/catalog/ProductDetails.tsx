caadda91796af89028a5b9193ba3e0b286601005
Sal Guled <SalmanGuled2@gmail.com>
2024-11-18 16:26:57 -0500

    Section 5 In Progress

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -3,34 +3,36 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Product } from "../../app/models/product";
+import agent from "../../app/api/agent";
 
 export default function ProductDetails() {
 
-    const {id} = useParams<{id: string}>();
+    const { id } = useParams<{ id: string }>();
     const [product, setProduct] = useState<Product | null>(null);
     const [loading, setLoading] = useState(true);
 
 
-    useEffect(() =>  {
-        axios.get(`http://localhost:5000/api/products/${id}`)
-        .then(response => setProduct(response.data))
-        .catch(error => console.log(error))
-        .finally(() => setLoading(false));
-        
+    useEffect(() => {
+
+        id && agent.Catalog.details(parseInt(id))
+            .then(response => setProduct(response))
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false));
+
     }, [id])
 
-    if(loading) return <h3>Loading...</h3>
+    if (loading) return <h3>Loading...</h3>
 
-    if(!product) return <h3>Product not found</h3>
+    if (!product) return <h3>Product not found</h3>
 
     return (
         <Grid2 container spacing={6}>
             <Grid2 size={6}>
-                <img src={product.pictureUrl} alt={product.name} style={{width: "100%"}}/>
+                <img src={product.pictureUrl} alt={product.name} style={{ width: "100%" }} />
             </Grid2>
             <Grid2 size={6}>
                 <Typography variant='h3'>{product.name}</Typography>
-                <Divider sx={{mb: 2}} />
+                <Divider sx={{ mb: 2 }} />
                 <Typography variant='h4' color='secondary'>${(product.price / 100).toFixed(2)}</Typography>
                 <TableContainer>
                     <Table>
@@ -62,7 +64,7 @@ export default function ProductDetails() {
 
 
                 </TableContainer>
-                
+
             </Grid2>
 
         </Grid2>

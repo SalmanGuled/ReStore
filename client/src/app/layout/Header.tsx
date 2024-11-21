73060418ec0ba8493c1369285cb9cb16ebd716f1
Sal Guled <SalmanGuled2@gmail.com>
2024-11-21 15:02:50 -0500

    Shopping cart feature working

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,6 +1,7 @@
 import { ShoppingCart } from "@mui/icons-material";
 import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
 import { Link, NavLink } from "react-router-dom";
+import { useStoreContext } from "../context/StoreContext";
 
 const midLinks = [
 
@@ -34,6 +35,9 @@ const navStyles = {
    }
 
 export default function Header({ darkMode, handleThemeChange }: Props) {
+
+    const {basket} = useStoreContext();
+    const itemCount = basket?.items.reduce((sum, item) => sum + item.quantity, 0);
 
     return (
 
@@ -69,7 +73,7 @@ export default function Header({ darkMode, handleThemeChange }: Props) {
 
                 <Box display='flex' alignItems='center'>
                 <IconButton component ={Link} to='/basket' size="large" edge='start' color='inherit' sx={{mr: 2}}>
-                    <Badge badgeContent='4' color='secondary'>
+                    <Badge badgeContent={itemCount} color='secondary'>
                         <ShoppingCart></ShoppingCart>
                     </Badge>
                 </IconButton>
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './app/layout/styles.css'
-import App from './app/layout/App.tsx'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router/Routes.tsx';
+import { StoreProvider } from './app/context/StoreContext.tsx';
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <StoreProvider>
+      <RouterProvider router={router} />
+    </StoreProvider>
+
   </StrictMode>,
 )
